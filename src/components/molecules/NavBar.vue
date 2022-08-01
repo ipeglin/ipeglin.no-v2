3c7046f6643d9a87e7e16d00d9ecd49d04abeb60
ipeglin <philip@eglin.no>
2022-08-01 17:33:44 +0200

    Added navbar component

---
+++ src/components/molecules/NavBar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div class="navigation">
+    <nav>
+      <ul>
+        <li>
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/cookbook">Cookbook</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/about">About</RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
