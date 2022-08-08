4ccef2995f9525d19502f15be33fa3e4a2400b65
ipeglin <philip@eglin.no>
2022-08-08 10:37:01 +0200

    Added sass support and styled button components

---
+++ frontend/src/components/atoms/Button.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+  type ButtonSizes = 'sm' | 'md' | 'lg';
+  
+interface Props {
+  onClick?: any,
+  size?: ButtonSizes,
+  text: string,
+  disabled?: boolean
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits(["click"])
+
+const buttonSize = `size-${props.size !== undefined ? props.size : 'md'}`;
+
+const handleClick = () => {
+  emit('click');
+}
+
+</script>
+
+<template>
+  <button :class="`button ${buttonSize} ${props.disabled ? 'disabled': ''}`" @click="handleClick">{{ props.text }}</button>
+</template>
+
+<style scoped lang="scss">
+
+  .button {
+    background: $color-button-primary;
+    border: none;
+    min-width: 64px;
+    width: 150px;
+    min-height: 48px;
+    border-radius: 8px;
+    font-size: 18px;
+    font-weight: medium;
+    cursor: pointer;
+    color: white;
+    white-space: nowrap;
+    box-shadow: 8px 8px 30px 0px $color-button-shadow;
+
+    &.disabled {
+      background: $color-white;
+      color: #525060;
+      box-shadow: none;
+    }
+
+    &.size-sm {
+      min-height: 40px; 
+    }
+
+    &.size-lg {
+      min-height: 56px;;
+    }
+  }
+
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from '@/components/atoms/Button.vue';
+
+const handleClick = () => {console.log('Clicked')}
+</script>
 
 <template>
   <main>
     <h1>Home view</h1>
+    <Button text="Small button" size="sm"/>
+    <Button text="Default button"/>
+    <Button text="Large button" size="lg"/>
+    <Button text="Default button" disabled/>
   </main>
 </template>
