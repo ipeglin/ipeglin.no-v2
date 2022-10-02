2bf9188973eea7b0c1c2985e29615c1642ab93da
ipeglin <philip@eglin.no>
2022-10-02 02:18:42 +0200

    Added LinkInterface instead of just string link

--- frontend/src/assets/interfaces/CardInterface.ts
+++ frontend/src/assets/interfaces/CardInterface.ts
@@ -3,6 +3,7 @@ import type { LinkInterface } from "./LinkInterface";
 export interface CardInterface {
   title: string,
   description: string,
+  tags: string[],
   image?: string,
-  link?: string
+  link?: LinkInterface
 }
