bebc8c23a1ba8a3545186a65bc0b41f9db21fb2c
ipeglin <philip@eglin.no>
2022-10-06 19:38:55 +0200

    Hydrate pinia github state before app mount

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,16 @@
   import { RouterView } from 'vue-router';
   import NavBar from './components/molecules/NavBar.vue';
   import Footer from './components/molecules/Footer.vue';
+  import { onBeforeMount } from 'vue';
+  import { useGitHubStore } from './stores/github';
+
+  const githubStore = useGitHubStore();
+
+  onBeforeMount(async () => {
+    await githubStore.fetchRepositories();
+    await githubStore.fetchFollowers();
+    await githubStore.fetchRepoImages();
+  });
 </script>
 
 <template>
