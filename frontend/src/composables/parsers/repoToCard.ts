8cad4129e6d4b569144b1340442861019378ac87
ipeglin <philip@eglin.no>
2022-10-06 23:42:00 +0200

    Map repo image to card

--- frontend/src/composables/parsers/repoToCard.ts
+++ frontend/src/composables/parsers/repoToCard.ts
@@ -1,8 +1,7 @@
 import type { CardInterface } from "@/assets/interfaces/CardInterface";
 import { reduceStringArrayToLengthLimit } from "./arrays";
 
-
-export const parseGitHubRepoToCard = (repoObject: any): CardInterface => {
+export const parseGitHubRepoToCard = (repoObject: any, imageUrl): CardInterface => {
   return {
     title: repoObject.name,
     description: repoObject.description,
@@ -11,11 +10,12 @@ export const parseGitHubRepoToCard = (repoObject: any): CardInterface => {
       name: 'View',
       value: repoObject.html_url
     },
+    image: imageUrl,
   }
 }
 
-export const parseGitHubRepoArrayToCard = (repoArray: any[]): CardInterface[] => {
+export const parseGitHubRepoArrayToCard = (repoArray: any[], repoImages = {}): CardInterface[] => {
   return repoArray.map((repo: any) => {
-    return parseGitHubRepoToCard(repo);
+    return parseGitHubRepoToCard(repo, repoImages[repo.name] || '');
   })
 }
