1915241a2006bafdca2eee4753587546120ada8a
Ian Philip Eglin <philip@eglin.no>
2023-03-13 12:34:35 +0100

    Dev (#18)
    * Changed deployment script to target frontend/ dir
    
    * Changed deploy
    
    * Deploy yml using yarn instead of npm
    
    * Added route alises to vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,17 @@
-import { fileURLToPath, URL } from 'node:url';
-
+// import { fileURLToPath, URL } from 'node:url';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import alias from '@rollup/plugin-alias';
+
+const projectRootDir = resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), alias()],
   base: '/ipeglin.no',
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+    alias: [{ find: '@', replacement: resolve(projectRootDir, 'src') }],
   },
   css: {
     preprocessorOptions: {
